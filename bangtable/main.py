cde110fe1c6aeb04b6c9d8932800a350bc583069
ito <nanisiyou1@gmail.com>
2024-10-17 16:26:37 +0900

    フラグ検知完了

--- bangtable/main.py
+++ bangtable/main.py
@@ -9,8 +9,11 @@ from animationclass import Animation
 
 animationA = Animation()
 animationB = Animation()
-stateA = State(animationA, 'data/data01.txt', 280)
-stateB = State(animationB, './data/data02.txt', 280)
+stateA = State(animationA, 'bangtable/data/data01.txt', 280)
+stateB = State(animationB, 'bangtable/data/data02.txt', 280)
+
+flag_A_path = 'bangtable/data/flag01.txt'
+flag_B_path = 'bangtable/data/flag02.txt'
 
 
 # 全部をこの関数内で定義する必要がある
@@ -27,9 +30,21 @@ async def run():
 				return
 			# ダイパントリガ(仮)
 			if (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE):
-				print("SPACE")
+				# ファイルを開く (書き込みモード 'w')
+				with open(flag_A_path, 'w', encoding='utf-8') as file:
+					file.write("True")
+
+
+		# 台パンフラグ読み込み
+		with open(flag_A_path, 'r+', encoding='utf-8') as file:
+			content = file.read()
+			if (content == "True"):
+				print("flag")
 				# 台パンデータセット、台パンフラグON
-				stateA.set_bang_data(random.randint(0, 999))
+				stateA.set_bang_flag()
+				# フラグリセット
+				file.seek(0)
+				file.write("False")
 
 		# ステート監視
 		stateA.BangObserver()
